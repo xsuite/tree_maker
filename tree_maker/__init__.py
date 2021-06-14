ad243c68e9b6ffaaab4e12f9ea11279227bf9b60
Guido Sterbini <guido.sterbini@cern.ch>
2021-06-14 14:40:52 +0000

    Adding tha package

---
+++ tree_maker/__init__.py
@@ -0,0 +1,17 @@
+"""
+list of sweet functions for data conversion and writing to disk
+
+"""
+
+__version__ = "0.0.1"
+
+from .NodeJob import NodeJob
+
+from .general import tree_from_yaml
+from .general import from_yaml
+
+
+
+
+
+
