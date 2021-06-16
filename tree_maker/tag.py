ef34fd772e3191710af69538f6876bfea4ced3a7
Hamish Graham <hamish.graham@cern.ch>
2021-06-16 09:10:31 +0000

    Update tag.py

--- tree_maker/tag.py
+++ tree_maker/tag.py
@@ -7,6 +7,7 @@ import datetime
 def read_yaml(myfile, verbose=False):
     """
     read_yaml is to read a yaml file and convert it into python.
+
     Example
     --------
     >>> read_yaml('mytest.yml')
@@ -48,6 +49,7 @@ def write_yaml(my_dict, myfile):
 def append_yaml(my_dict, myfile):
     '''
     append_yaml is to append dictionaries to a yaml file.
+    
     Examples
     --------
     >>> append_yaml({'blue': 'bonjour'}, ('mytest2.yml'))
