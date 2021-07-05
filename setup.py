a3196f270c3e3ed8987759af96885d1fd11d16bc
Guido Sterbini <sterbini@hpc-201-11-01-a.cr.cnaf.infn.it>
2021-07-05 08:58:06 +0200

    adding orjson and updating the .gitignore

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with (HERE / "README.md").open("rt") as fh:
 
 
 REQUIREMENTS: dict = {
-    "core": ["anytree", "pyyaml", "ruamel.yaml", "pathlib", "pandas"],
+    "core": ["anytree", "pyyaml", "ruamel.yaml", "pathlib", "pandas","orjson"],
     "test": [
         "pytest",
     ],
